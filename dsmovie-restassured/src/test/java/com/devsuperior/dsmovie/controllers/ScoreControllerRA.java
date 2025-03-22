4d513ec2e05ce04c1c779d40d14e00252232af21
Leonardo <leomeireles66@gmail.com>
2025-03-22 09:17:30 -0300

    Score controller tests passed

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,18 +1,88 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 public class ScoreControllerRA {
-	
+
+	private Long existingMovieId, nonExistingMovieId;
+	private String adminUsername, adminPassword, adminToken;
+	private Map<String, Number> putScoreInstance;
+
+	@BeforeEach
+	void setUp() throws Exception{
+		baseURI = "http://localhost:8080";
+		existingMovieId = 1L;
+		nonExistingMovieId = 50L;
+
+		adminUsername = "maria@gmail.com";
+		adminPassword = "123456";
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+
+		putScoreInstance = new HashMap<>();
+		putScoreInstance.put("movieId", existingMovieId);
+		putScoreInstance.put("score", 4);
+
+	}
+
 	@Test
-	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+		putScoreInstance.put("movieId", nonExistingMovieId);
+		JSONObject putScore = new JSONObject(putScoreInstance);
+		given()
+			.header("Content-Type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.body(putScore)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.put("/scores")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+
 	}
 	
 	@Test
 	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+		putScoreInstance.put("movieId", null);
+		JSONObject putScore = new JSONObject(putScoreInstance);
+		given()
+			.header("Content-Type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.body(putScore)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.put("/scores")
+		.then()
+				.statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value());
 	}
 	
 	@Test
-	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
+	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+		putScoreInstance.put("score", -1);
+		JSONObject putScore = new JSONObject(putScoreInstance);
+		given()
+			.header("Content-Type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.body(putScore)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.put("/scores")
+		.then()
+				.statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value());
 	}
 }
